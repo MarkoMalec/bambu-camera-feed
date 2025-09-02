ce064b612e119f1e8bc19f89da505d32e9ef993a
MarkoMalec <cropsychooo@yahoo.com>
2025-09-02 18:11:36 +0200

    .

--- src/components/add-feed.tsx
+++ src/components/add-feed.tsx
@@ -1,15 +1,10 @@
 "use client";
-import { addNewFeed } from "~/utils/feedActions";
 
 const AddFeedButton = () => {
-  const handleClick = () => {
-    
-  };
 
   return (
     <button
       className="rounded bg-[#900] px-4 py-2 text-white"
-      onClick={handleClick}
     >
       Add Feed
     </button>
